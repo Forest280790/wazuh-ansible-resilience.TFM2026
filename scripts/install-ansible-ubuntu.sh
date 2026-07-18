d5546de612f29748386668de023621d90aa1eaf7
Carlos Florez <cflorezr28@gmail.com>
2026-07-18 00:27:14 -0500

    Update install-ansible-ubuntu.sh for Ansible setup
    Added system update and Ansible installation steps.

--- scripts/install-ansible-ubuntu.sh
+++ scripts/install-ansible-ubuntu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Actualización del sistema e instalación del motor core de Ansible
+echo "Actualizando repositorios e instalando dependencias..."
 sudo apt update
 sudo apt install ansible python3-pip -y
+
+# Instalación del conector WinRM para interoperabilidad Linux-to-Windows
 pip3 install pywinrm
+echo "Entorno de control listo."
